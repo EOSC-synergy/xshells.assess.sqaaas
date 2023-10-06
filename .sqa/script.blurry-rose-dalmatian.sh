e6263c040126581fa2d2a57142981299ec18beea
orviz <orviz@ifca.unican.es>
2023-10-06 16:26:15 +0200

    Add JePL file structure

---
+++ .sqa/script.blurry-rose-dalmatian.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gricad-gitlab.univ-grenoble-alpes.fr/schaeffn/xshells &&
+    cffconvert --validate
+)
